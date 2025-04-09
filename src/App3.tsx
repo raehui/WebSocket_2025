26e958e696d3b31bb047127cae735cb69ea7ed9b
KWAK RAEHUI <glasofk12345@naver.com>
2025-04-09 14:29:38 +0900

    채팅방 구현중중

--- src/App3.tsx
+++ src/App3.tsx
@@ -1,78 +1,92 @@
-// src/App2.tsx
-
 import { useEffect, useRef, useState } from 'react'
 import { v4 as uuid } from 'uuid';
 import { useWebSocket } from './hooks/useWebSocket';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 
-interface Message {
-  id: string;
-  content: string;
+interface Message{
+  id:string;
+  content:string;
 }
 
-function App3() {
 
+
+function App3() {
+  
   const [msgs, setMsgs] = useState<Message[]>([]);
-  const inputRef = useRef<HTMLInputElement>(null);
-  // useWebSocket() hook 사용해서 웹소켓 연결하기
-  // object 분할할당
-  const { sendMessage, connected } = useWebSocket("ws://192.168.0.107:9000/ws", {
-    onOpen: () => {
+  const inputRef=useRef<HTMLInputElement>(null);
+  //대화방에 입장한 userName 도 상태값으로 관리하기
+  const [userName, setUserName]=useState<string>();
+  //대화방 참여자 목록도 상태값으로 관리
+  const [userList, setUserList]=useState<string[]>([]);
+
+  // useWebSocket() hook 사용해서 웹소켓 연결하기 
+  const {sendMessage, connected} = useWebSocket("ws://192.168.0.107:9000/ws", {
+    onOpen:()=>{
       console.log("연결됨!");
     },
-    onMessage: (event) => {
-      // setMsgs((prevState) => [...prevState, { id: uuid(), content: event.data }]);
-      // 응답된 json 문자열을 실제 object 로 변경한다.
+    onMessage:(event)=>{
+      //응답된 json 문자열을 실제 object로 변경한다.
       const received = JSON.parse(event.data);
       if(received.type === "enter"){
         setIsEnter(true);
         setMsgs(prevState=>{
-          const msg= received.payload.userName + "님이 입장했습니다!";
-          return [...prevState,{id: uuid(), content:msg}]
+          const msg = received.payload.userName+" 님이 입장했습니다.";
+          return [...prevState, {id:uuid(), content:msg}]
         });
-      }else if(received.type === "leave"){
-
-      }else if(received.type === "newMessage"){
-
+        //사용자 목록을 update 한다.
+        setUserList(received.payload.userList);
+      }else if(received.type == "leave"){
+        const msg = received.payload.userName+" 님이 퇴장했습니다.";
+        setMsgs(prevState => [...prevState, {id:uuid(), content:msg}]);
+        //leave 된 userName 을 userList 에서 제거한다
+        setUserList(prevState => prevState.filter(item => item !== received.payload.userName))
+      }else if(received.type ==="public"){
+        setMsgs(prevState=>{
+          //출력할 메세지를 구성한다.
+          const msg=`${received.payload.userName} : ${received.payload.text}`;
+          return [...prevState, {id:uuid(), content:msg}];
+        });
       }
     },
-    onClose: () => {
-      console.log("연결 종료!");
+    onClose:()=>{
+      console.log("연결끊김!");
     }
   });
+  
 
-  const handleSend = () => {
+  const handleSend=()=>{
     //입력한 메세지 읽어와서
-    const msg = inputRef.current?.value;
-    // 서버에 전송할 정보를 담고 있는 odject
-    const obj = {
-      path: "/chat/send",
-      data: {
-        text: msg
+    const msg=inputRef.current?.value;
+    //서버에 전송할 정보를 담고 있는 object
+    const obj={
+      path:"/chat/public",
+      data:{
+        userName,
+        text:msg
       }
-    }
-    // object 를 json 문자열로 변환해서 전송하기
+    };
+
+    //object를 json문자열로 변환해서 전송하기
     sendMessage(JSON.stringify(obj));
     //입력창 초기화
-    inputRef.current!.value = "";
+    inputRef.current!.value="";
   }
-
-  const divStyle = {
-    height: "300px",
-    width: "500px",
-    backgroundColor: "#cecece",
-    padding: "10px",
-    overflowY: "auto",
-    scrollBehavior: "smooth"
+  const divStyle={
+    height:"300px",
+    backgroundColor:"#cecece",
+    padding:"10px",
+    overflowY:"auto",
+    scrollBehavior:"smooth"
   };
-  const divRef = useRef<HTMLDivElement>(null);
+  const divRef=useRef<HTMLDivElement>(null);
   //자동 스크롤
-  // 처음에 div 가 안 만들어져 있을 수도 있느니 조건문으로 에러 방지
-  useEffect(() => {
-    if (divRef.current) {
-      divRef.current!.scrollTop = divRef.current!.scrollHeight;
-    }
+  useEffect(()=>{
+      if(divRef.current){
+       divRef.current!.scrollTop = divRef.current!.scrollHeight;
+      }
   }, [msgs]);
 
+  //풍선스타일
   const bubbleStyle: React.CSSProperties = {
     backgroundColor: "#fff",
     borderRadius: "10px",
@@ -82,43 +96,60 @@ function App3() {
     maxWidth: "80%",
     boxShadow: "0 1px 4px rgba(0,0,0,0.2)"
   };
-  // 대화방에 입장했는지 여부 
+
+  //대화방에 입장했는지 여부
   const [isEnter, setIsEnter] = useState<boolean>(false);
 
   const inputUserRef = useRef<HTMLInputElement>(null);
-  const handleEnter = () =>{
-    const obj ={
-      // 아이디를 웹소켓에 보내면 아이디에 맞는 session 을 찾아서 매칭
-      path:"/chat/enter",
-      data:{
-        userName:inputUserRef.current?.value
-      }
+  const handleEnter = ()=>{
+    const obj={
+        path:"/chat/enter", 
+        data:{
+          userName:inputUserRef.current?.value 
+        }
     };
     sendMessage(JSON.stringify(obj));
+    //userName 을 상태값에 넣어주기
+    setUserName(obj.data.userName);
   }
 
   return (
-    <div>
-      <h1>WebSocket 테스트</h1>
-      <h2>WebSocket {connected ? "✅ 연결됨" : "❌ 끊김"}</h2>
-      {isEnter ?
-        <>
-          <input type="text" ref={inputRef} />
-          <button onClick={handleSend}>전송</button>
-          <div style={divStyle} ref={divRef}>
-            {msgs.map(item => (
-              <div key={item.id}>
-                <div style={bubbleStyle}>{item.content}</div>
-              </div>
-            ))}
-          </div>
-        </>
-        :
-        <>
-          <input type='text' placeholder='userName 입력...' ref={inputUserRef} />
-          <button onClick={handleEnter}>입장</button>
-        </>
-      }
+    <div className='container'>
+      <h1>WebSocket 테스트3</h1>
+      <h2>WebSocket {connected ? "✅ 연결됨" : "❌ 끊김"} {userName}</h2>
+         { isEnter ?
+            <div className='row'>
+                <div className="col-8">
+                  <div style={divStyle} ref={divRef}>
+                    {msgs.map(item => (
+                        <div key={item.id}>
+                          <div style={bubbleStyle}>{item.content}</div>
+                        </div>
+                    ))}
+                  </div>
+                  <InputGroup className="mb-3">
+                    <Form.Control placeholder="대화 입력..." ref={inputRef}/>
+                    <Button variant="outline-secondary" onClick={handleSend}>Send</Button>
+                  </InputGroup>
+                  
+                </div>
+                <div className='col-4'>
+                  <h3>참여자 목록</h3>
+                  <ul>
+                    {userList.map(item => 
+                      <li key={item}>
+                         <button>{item}</button> 
+                      </li> 
+                    )}
+                  </ul>
+                </div> 
+            </div>
+            :
+            <>
+               <input ref={inputUserRef} type="text" placeholder='UserName 입력...' />
+               <button onClick={handleEnter}>입장</button>
+            </>
+         }
     </div>
   )
 }
