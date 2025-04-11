08f5e5275b8b0f40071429a2165bda4d9d0f2565
KWAK RAEHUI <glasofk12345@naver.com>
2025-04-11 11:46:46 +0900

    채팅방 이미지 업로드 및 클립보드드

--- src/App3.tsx
+++ src/App3.tsx
@@ -2,76 +2,83 @@ import { useEffect, useRef, useState } from 'react'
 import { v4 as uuid } from 'uuid';
 import { useWebSocket } from './hooks/useWebSocket';
 import { Button, Form, InputGroup, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
 
-interface Message {
-  id: string;
-  content: string;
-  sender?: string; // 이 메세지를 누가 보냈는지 정보로 Message 객체에 담기 위해
+interface Message{
+  id:string;
+  content:string;
+  sender?:string; //이 메세지를 누가 보냈는지 정보도 Message 객체에 담기 위해 
+  isImage?:boolean; //이 메세지가 이미지인지 여부 
 }
 
 
 
 function App3() {
-
+  
   const [msgs, setMsgs] = useState<Message[]>([]);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef=useRef<HTMLInputElement>(null);
   //대화방에 입장한 userName 도 상태값으로 관리하기
-  const [userName, setUserName] = useState<string>();
-  // userName 을 userRef()를 이용해서 관리하기 ??? 실시간으로 반영 관리를 하기위해서
+  const [userName, setUserName]=useState<string>();
+  // userName 을 useRef() 를 이용해서 관리하기
   const userNameRef = useRef<string|null>(null);
   //대화방 참여자 목록도 상태값으로 관리
-  const [userList, setUserList] = useState<string[]>([]);
+  const [userList, setUserList]=useState<string[]>([]);
 
   // useWebSocket() hook 사용해서 웹소켓 연결하기 
-  const { sendMessage, connected } = useWebSocket("ws://192.168.0.107:9000/ws", {
-    onOpen: () => {
+  const {sendMessage, connected} = useWebSocket("ws://192.168.0.107:9000/ws", {
+    onOpen:()=>{
       console.log("연결됨!");
     },
-    onMessage: (event) => {
+    onMessage:(event)=>{
       //응답된 json 문자열을 실제 object로 변경한다.
-      // event.data 은 type, payload
       const received = JSON.parse(event.data);
-      if (received.type === "enter") {
+      if(received.type === "enter"){
         setIsEnter(true);
-        setMsgs(prevState => {
-          const msg = received.payload.userName + " 님이 입장했습니다.";
-          return [...prevState, { id: uuid(), content: msg }]
+        setMsgs(prevState=>{
+          const msg = received.payload.userName+" 님이 입장했습니다.";
+          return [...prevState, {id:uuid(), content:msg}]
         });
         //사용자 목록을 update 한다.
         setUserList(received.payload.userList);
-      } else if (received.type == "leave") {
-        const msg = received.payload.userName + " 님이 퇴장했습니다.";
-        setMsgs(prevState => [...prevState, { id: uuid(), content: msg }]);
+      }else if(received.type == "leave"){
+        const msg = received.payload.userName+" 님이 퇴장했습니다.";
+        setMsgs(prevState => [...prevState, {id:uuid(), content:msg}]);
         //leave 된 userName 을 userList 에서 제거한다
         setUserList(prevState => prevState.filter(item => item !== received.payload.userName))
-      } else if (received.type === "public") {
-        setMsgs(prevState => {
+      }else if(received.type ==="public"){
+        setMsgs(prevState=>{
           //출력할 메세지를 구성한다.
-          const msg = received.payload.text;
-          return [...prevState, { id: uuid(), content: msg, sender: received.payload.userName }];
+          const msg=received.payload.text;
+          return [...prevState, {id:uuid(), content:msg, sender:received.payload.userName}];
         });
       }else if(received.type === "whisper"){
-        // 여기가 실행되는 경우는 귓말을 보낸 사람과, 받는 사람이다. ???
-        // 
+        //여기가 실행되는 경우는 귓말을 보낸 사람과, 받는 사람이다.
         const msg = received.payload.userName === userNameRef.current ? 
           `${received.payload.text} => [귓말] ${received.payload.toUserName}`
         :
-          `[귓말] =>  ${received.payload.text}`
+          `[귓말] => ${received.payload.text}`
         ;
-        setMsgs(prevState=>[...prevState, {id:uuid(), content: msg, sender:received.payload.userName}]);
+        setMsgs(prevState=>[...prevState, {id:uuid(), content:msg, sender:received.payload.userName}]);
+      }else if(received.type === "image"){
+        setMsgs(prevState => [...prevState, {
+          id:uuid(),
+          content: `/upload/${received.payload.saveFileName}`,
+          isImage: true,
+          sender: received.payload.userName
+        }]);
       }
     },
-    onClose: () => {
+    onClose:()=>{
       console.log("연결끊김!");
     }
   });
-
-  // 메세지 보내는 함수
-  const handleSend = () => {
+  
+  //메세지 보내는 함수 
+  const handleSend=()=>{
     //입력한 메세지 읽어와서
-    const msg = inputRef.current?.value;
+    const msg=inputRef.current?.value;
     //서버에 전송할 정보를 담고 있는 object
-    let obj = null;
+    let obj=null;
     if(selectedUser){
       obj={
         path:"/chat/whisper",
@@ -88,27 +95,26 @@ function App3() {
           userName,
           text:msg
         }
-      };
+      }
     }
-
     //object를 json문자열로 변환해서 전송하기
     sendMessage(JSON.stringify(obj));
     //입력창 초기화
-    inputRef.current!.value = "";
+    inputRef.current!.value="";
   }
-  const divStyle = {
-    height: "300px",
-    backgroundColor: "#cecece",
-    padding: "10px",
-    overflowY: "auto",
-    scrollBehavior: "smooth"
+  const divStyle={
+    height:"500px",
+    backgroundColor:"#cecece",
+    padding:"10px",
+    overflowY:"auto",
+    scrollBehavior:"smooth"
   };
-  const divRef = useRef<HTMLDivElement>(null);
+  const divRef=useRef<HTMLDivElement>(null);
   //자동 스크롤
-  useEffect(() => {
-    if (divRef.current) {
-      divRef.current!.scrollTop = divRef.current!.scrollHeight;
-    }
+  useEffect(()=>{
+      if(divRef.current){
+       divRef.current!.scrollTop = divRef.current!.scrollHeight;
+      }
   }, [msgs]);
 
   const bubbleStyleBase: React.CSSProperties = {
@@ -120,14 +126,14 @@ function App3() {
     fontSize: "0.95rem",
     lineHeight: "1.4",
   };
-  // 내가 보낸 메세지 스타일
+  //내가 보낸 메세지 스타일 
   const myBubbleStyle: React.CSSProperties = {
     ...bubbleStyleBase,
     backgroundColor: "#DCF8C6", // 연한 연두색 (WhatsApp 스타일)
     alignSelf: "flex-end",
     color: "#000",
   };
-  // 다른 사람이 보내 메세지 스타일
+  //다른 사람이 보낸 메세지 스타일 
   const otherBubbleStyle: React.CSSProperties = {
     ...bubbleStyleBase,
     backgroundColor: "#fff",
@@ -140,96 +146,167 @@ function App3() {
   const [isEnter, setIsEnter] = useState<boolean>(false);
 
   const inputUserRef = useRef<HTMLInputElement>(null);
-  const handleEnter = () => {
-    const obj = {
-      path: "/chat/enter",
-      data: {
-        userName: inputUserRef.current?.value
-      }
+  
+  const handleEnter = ()=>{
+    const obj={
+        path:"/chat/enter", 
+        data:{
+          userName:inputUserRef.current?.value 
+        }
     };
     sendMessage(JSON.stringify(obj));
     //userName 을 상태값에 넣어주기
     setUserName(obj.data.userName);
-    // userName 을 userNameRef 에도 넣어주기
-    userNameRef.current = inputUserRef.current!.value;
+    //userName 을 userNameRef 에도 넣어주기
+    userNameRef.current = inputUserRef.current!.value ;
   }
-
-  // 메세지를 보낸 사람을 출력할 스타일 
-  const senderStyle = {
-    fontSize: "0.75rem",
-    fontWeight: "bold",
-    marginBottom: "2px",
-    color: "#555"
+  //메세지를 보낸 사람을 출력할 스타일
+  const senderStyle={
+    fontSize:"0.75rem",
+    fontWeight:"bold",
+    marginBottom:"2px",
+    color:"#555"
   };
-  // 입장 , 퇴장 메세지 스타일
-  const infoStyle = {
-    textAlign: "center",
-    margin: "5px 0",
-    fontStyle: "italic",
+  //입장, 퇴장 메세지 스타일
+  const infoStyle={
+    textAlign:"center",
+    margin:"5px 0",
+    fontStyle:"italic",
     color: "#888"
   }
-  // 귓말 보내기 위해 선택된 username 을 상태값으로 관리
-  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  // 귓말 보내기 위해 선택된 userName 을 상태값으로 관리
+  const [selectedUser, setSelectedUser] = useState<string|null>(null);
+  // input type="file" 의 참조값
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const handleImageClick = ()=>{
+    // input type="file" 요소를 강제 클릭해서 이미지를 선택할수 있도록 한다. 
+    fileInputRef.current?.click();
+  };
+  //이미지 파일을 선택했을때 실행되는 함수 
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>)=>{
+    //선택된 파일 객체
+    const file = e.target.files?.[0];
+    fileUpload(file);
+  }
+  //매개변수에 전달된 파일 객체를 업로드 하는 함수 
+  const fileUpload = async (file:File|undefined|null)=>{
+    if(!file)return;
+    //FormData 
+    const formData = new FormData();
+    formData.append("image", file);
+    //axios 를 이용해서 multipart/form-data 요청해서 이미지 업로드
+    try{
+      const response = await axios.post("/api/image", formData, {
+        headers:{"Content-Type":"multipart/form-data"}
+      });
+      console.log(response.data);
+      
+      // response.data 는 {saveFileName:"xxx.png"}
+      // 웹소켓을 이용해서 서버에 업로드된 파일 정보를 전송한다.
+      const obj={
+        path:"/chat/image",
+        data:{
+          userName,
+          saveFileName:response.data.saveFileName
+        }
+      }
+      sendMessage(JSON.stringify(obj));
+     
+    }catch(err){
+      console.log("업로드 실패!", err);
+    }
+  }
+
+  // input 요소에 "paste" 이벤트 처리하는 함수
+  const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>)=>{
+    //붙여 넣기한 item 목록 얻어내기
+    const items = e.clipboardData.items;
+    //반복문 돌면서
+    for(let i=0; i<items.length; i++){
+      const item=items[i];
+      if(item.kind === "file" && item.type.startsWith("image/")){
+        //실제 파일객체로 얻어낸다.
+        const file=item.getAsFile();
+        fileUpload(file);
+      }
+    }
+  };
 
   return (
     <div className='container'>
       <h1>WebSocket 테스트3</h1>
       <h2>WebSocket {connected ? "✅ 연결됨" : "❌ 끊김"} {userName}</h2>
-      {isEnter ?
-        <div className='row'>
-          <div className="col-8">
-            <div style={divStyle} ref={divRef}>
-              {msgs.map(item => (
-                item.sender ?
-                  <div key={item.id} style={{
-                    display: "flex",
-                    flexDirection: "column", // 자식이 세로 방향으로 쌓임
-                    alignItems: item.sender === userName ? "flex-end" : "flex-start", // 좌 우 결정(flexDirection 에 따라서 위아래 좌우인지 결정)
-                    marginBottom: "10px"
-                  }}>
-                    {/* 메세지 분기 */}
-                    {item.sender !== userName && <div style={senderStyle}>{item.sender}</div>}
-                    <div style={item.sender !== userName ? otherBubbleStyle : myBubbleStyle}>
-                      {item.content}
-                    </div>
+         { isEnter ?
+            <div className='row'>
+                <div className="col-8">
+                  <div style={divStyle} ref={divRef}>
+                    {msgs.map(item => (
+                        item.sender ?
+                          <div key={item.id} style={{
+                            display:"flex",
+                            flexDirection:"column",
+                            alignItems: item.sender === userName ? "flex-end" : "flex-start",
+                            marginBottom: "10px"
+                          }}>
+                            { item.sender !== userName && <div style={senderStyle}>{item.sender}</div>}
+                            <div style={item.sender !== userName ? otherBubbleStyle : myBubbleStyle}>
+                            { 
+                              item.isImage ? 
+                              <img src={item.content}
+                                style={{maxWidth:"200px", borderRadius:"10px"}}
+                                alt="업로드된 이미지"/> 
+                              : 
+                              item.content
+                            }
+                            </div>
+                          </div>
+                        :
+                          <div key={item.id} style={infoStyle}>
+                            {item.content}  
+                          </div>
+                    ))}
                   </div>
-                  :
-                  <div key={item.id} style={infoStyle}>
-                    {item.content}
-                  </div>
-              ))}
+                  <InputGroup className="mb-3">
+                    <Form.Control 
+                      placeholder={selectedUser ? selectedUser+" 님에게 귓말 보내기..." : "대화 입력..."} 
+                      ref={inputRef}
+                      onKeyDown={(e)=>{
+                        //Enter 키를 눌렀을때 handleSend() 함수 호출하기 
+                        if(e.key === "Enter")handleSend();
+                      }}
+                      onPaste={handlePaste}
+                    />
+                    <Button variant='outline-success' onClick={handleImageClick}>이미지</Button>
+                    <Button variant="outline-secondary" onClick={handleSend}>Send</Button>
+                  </InputGroup>
+                  <input type="file" 
+                    accept='image/*' 
+                    ref={fileInputRef} 
+                    style={{display:"none"}}
+                    onChange={handleFileChange}/>
+                </div>
+                <div className='col-4'>
+                  <h3>참여자 목록</h3>
+                  <ListGroup as="ul">
+                    {userList.map(item => 
+                      <ListGroup.Item as="li"
+                        key={uuid()}
+                        action
+                        style={{cursor:"pointer"}}
+                        active={item === selectedUser}
+                        onClick={()=>setSelectedUser(item === selectedUser ? null : item)}>
+                        {item}
+                      </ListGroup.Item>
+                    )}
+                  </ListGroup>
+                </div> 
             </div>
-            <InputGroup className="mb-3">
-              <Form.Control placeholder={selectedUser ? selectedUser + "님에게 귓말 보내기...": "대화 입력..."}
-                ref={inputRef}
-                onKeyDown={(e) => {
-                  // Enter key 를 눌렀을 때 handleSend() 함수 호출하기
-                  if (e.key === "Enter") handleSend();
-                }}
-              />
-              <Button variant="outline-secondary" onClick={handleSend}>Send</Button>
-            </InputGroup>
-
-          </div>
-          <div className='col-4'>
-            <h3>참여자 목록</h3>
-            <ListGroup as="ul">
-              {userList.map(item =>
-                <ListGroup.Item as="li"
-                  action style={{ cursor: "pointer" }}
-                  active={item === selectedUser}
-                  onClick={() => setSelectedUser(item === selectedUser ? null : item)}>
-                  {item}
-                </ListGroup.Item>)}
-            </ListGroup>
-          </div>
-        </div>
-        :
-        <>
-          <input ref={inputUserRef} type="text" placeholder='UserName 입력...' />
-          <button onClick={handleEnter}>입장</button>
-        </>
-      }
+            :
+            <>
+               <input ref={inputUserRef} type="text" placeholder='UserName 입력...' />
+               <button onClick={handleEnter}>입장</button>
+            </>
+         }
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    open:true,
+    proxy:{
+      "/api":{
+        target:"http://192.168.0.107:9000",
+        changeOrigin:true                
+      },
+      "/upload":{
+        target:"http://192.168.0.107:9000",
+        changeOrigin:true                
+      }
+    }
+  
+  }
 })
